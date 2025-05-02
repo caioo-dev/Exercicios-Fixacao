028b8db16cd1490f15ab80d7f2ef993d90899346
Caio Andrade <caio.andrade2442@gmail.com>
2025-05-01 21:17:33 -0300

    Membros estaticos

---
+++ Calculadora/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double n1 = 3.20;
+            double n2 = 4.50;
+
+            Console.WriteLine(Soma(n1, n2));
+
+        }
+
+        static double Soma(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
