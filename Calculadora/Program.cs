42ebea83de0b0b9d2b3f36e9b646774d50d33ed6
Caio Andrade <caio.andrade2442@gmail.com>
2025-05-09 21:53:58 -0300

    calc

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,20 +1,64 @@
 ﻿namespace Calculadora
 {
+
+    class Calculadora
+    {
+        public static double Soma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Subtracao(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Multiplicacao(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public static double Divisao(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            double n1 = 3.20;
-            double n2 = 4.50;
-
-            Console.WriteLine(Soma(n1, n2));
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("Escolha uma operação:");
+            Console.WriteLine("1. Soma");
+            Console.WriteLine("2. Subtração");
+            Console.WriteLine("3. Multiplicação");
+            Console.WriteLine("4. Divisão");
+            int opcao = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro número:");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            double n2 = double.Parse(Console.ReadLine());
+            double resultado = 0;
+            switch (opcao)
+            {
+                case 1:
+                    resultado = Calculadora.Soma(n1, n2);
+                    break;
+                case 2:
+                    resultado = Calculadora.Subtracao(n1, n2);
+                    break;
+                case 3:
+                    resultado = Calculadora.Multiplicacao(n1, n2);
+                    break;
+                case 4:
+                    resultado = Calculadora.Divisao(n1, n2);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    return;
+            }
+            Console.WriteLine($"Resultado: {resultado}");
 
         }
 
-        static double Soma(double firstNumber, double secondNumber)
-        {
-            return firstNumber + secondNumber;
-        }
     }
 }
