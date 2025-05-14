65d2c2821d1bf025aa22ea9681f32d54ef0ebf20
Caio Andrade <caio.andrade2442@gmail.com>
2025-05-13 21:34:53 -0300

    exercicio treino listas

---
+++ Employees/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Employees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many employees will be registered? ");
+            int N = int.Parse(Console.ReadLine());
+
+            List<Employee> employeeList = new List<Employee>();
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                while (employeeList.Exists(e => e.Id == id))
+                {
+                    Console.Write("This id already exists. Try again: ");
+                    id = int.Parse(Console.ReadLine());
+                }
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                employeeList.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee emp = employeeList.Find(x => x.Id == searchId);
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.increaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exists");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee obj in employeeList)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
