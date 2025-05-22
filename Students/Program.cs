d002959978d951610f0c027415b2c69cfbcf02d4
Caio Andrade <caio.andrade2442@gmail.com>
2025-05-21 22:22:57 -0300

    exercicio de cadastro de alunos e notas

---
+++ Students/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Students
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Caio", 1, [2.23, 5.32, 34.3]);
+
+            Student.AverageGrades(s1.Grades);
+
+            s1.AddGrades([2.22]);
+        }
+    }
+}
---
+++ Students/Student.cs
@@ -0,0 +1,37 @@
+﻿namespace Students
+{
+    internal class Student
+    {
+        public string Name { get; private set; }
+        public int RegistrationNumber { get; private set; }
+        public double[] Grades { get; private set; }
+
+        public Student(string name, int registrationNumber)
+        {
+            Name = name;
+            RegistrationNumber = registrationNumber;
+        }
+
+        public Student(string name, int registrationNumber, double[] grades) : this(name, registrationNumber)
+        {
+            Grades = grades;
+        }
+
+        public void AddGrades(double[] grades)
+        {
+            Grades = grades;
+        }
+
+        public static void AverageGrades(double[] grades)
+        {
+            double total = 0;
+
+            for(int i = 0; i < grades.Length; i++)
+            {
+                total += grades[i];
+            }
+
+            Console.WriteLine(total / grades.Length);
+        }
+    }
+}
